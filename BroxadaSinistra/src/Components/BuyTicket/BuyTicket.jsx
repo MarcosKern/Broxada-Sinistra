59545eaea823e42a1012ea082c81eff85fa4149e
MarcosKern <vkg.marcos@gmail.com>
2023-07-07 20:51:11 -0300

    fix minor responsivities problems

--- BroxadaSinistra/src/Components/BuyTicket/BuyTicket.jsx
+++ BroxadaSinistra/src/Components/BuyTicket/BuyTicket.jsx
@@ -1,5 +1,5 @@
 export default function BuyTicket() {
   return (
-    <h3 id="buy"><a href="https://wa.me/5511914646248">comprar ingresso</a></h3>
+    <h3 id="buy"><a href="https://wa.me/5511914646248" target="_blank" rel="noreferrer">comprar ingresso</a></h3>
   )
 }
--- BroxadaSinistra/src/Components/Navbar/Navbar.jsx
+++ BroxadaSinistra/src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <a href="#mosaico">conheça o ambiente</a>
       <a href="#mosaico-image-11">nossos apoiadores</a>
       <a href="#supporters-section">Mais informações</a>
-      <a href="#buy">Garanta seu ingresso</a>
+      <a href="https://wa.me/5511914646248" target='_blank' rel="noreferrer">Garanta seu ingresso</a>
     </nav>
   )
 }
