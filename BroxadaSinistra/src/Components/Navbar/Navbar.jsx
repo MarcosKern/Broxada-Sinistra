02c1eb393d015cfe26a7bf2f76eaf445e7f53fa3
MarcosKern <vkg.marcos@gmail.com>
2023-08-29 19:28:16 -0300

    last entry

--- BroxadaSinistra/src/Components/Navbar/Navbar.jsx
+++ BroxadaSinistra/src/Components/Navbar/Navbar.jsx
@@ -3,10 +3,13 @@ import './navbar.css'
 export default function Navbar() {
   return (
     <nav>
-      <a href="#mosaico">conheça o ambiente</a>
-      <a href="#mosaico-image-11">nossos apoiadores</a>
-      <a href="#supporters-section">Mais informações</a>
-      <a href="https://wa.me/5511914646248" target='_blank' rel="noreferrer">Garanta seu ingresso</a>
+      <a
+        href="https://wa.me/5511914646248"
+        target='_blank'
+        rel="noreferrer"
+      >
+        últimos ingressos, garanta o seu!
+      </a>
     </nav>
   )
 }
